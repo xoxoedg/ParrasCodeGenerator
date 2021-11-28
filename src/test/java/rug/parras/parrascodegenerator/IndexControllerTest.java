0ba0c711f7d53f0f8d80ac2137037c1df1a1d098
xoxoedg <rugyasin@gmail.com>
2021-11-28 18:23:23 +0100

    Added Mockito Dependency and added Test to IndexController

--- src/test/java/rug/parras/parrascodegenerator/IndexControllerTest.java
+++ src/test/java/rug/parras/parrascodegenerator/IndexControllerTest.java
@@ -1,17 +1,21 @@
 package rug.parras.parrascodegenerator;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 
 class IndexControllerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
     @Test
-    void getIndexPage() {
+    void getIndexPage() throws Exception {
+        MockMvc mvc = MockMvcBuilders.standaloneSetup(new IndexController()).build();
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 }
