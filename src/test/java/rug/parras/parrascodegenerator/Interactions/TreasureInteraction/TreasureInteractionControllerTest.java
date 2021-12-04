7d69cf7a33cd1234122e2f464cfdf83617f85587
xoxoedg <rugyasin@gmail.com>
2021-12-04 13:00:15 +0100

    Added tests for TreasureController

---
+++ src/test/java/rug/parras/parrascodegenerator/Interactions/TreasureInteraction/TreasureInteractionControllerTest.java
@@ -0,0 +1,49 @@
+package rug.parras.parrascodegenerator.Interactions.TreasureInteraction;
+
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class TreasureInteractionControllerTest {
+
+    @Mock
+    private TreasureInteractionService treasureInteractionService;
+
+    @InjectMocks
+    private TreasureInteractionController treasureInteractionController;
+
+
+    @Test
+    void getTreasurePage() throws Exception {
+        MockMvc mvcControllerGet = MockMvcBuilders.standaloneSetup(treasureInteractionController).build();
+        mvcControllerGet.perform(get("/treasure"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("treasureGeneratorPage"))
+                .andExpect(model().attribute("treasureInteraction", Matchers.any(Treasure.class)));
+
+    }
+
+    @Test
+    void treasureSubmit() throws Exception {
+        MockMvc mvcControllerPost = MockMvcBuilders.standaloneSetup(treasureInteractionController).build();
+        mvcControllerPost.perform(post("/treasure"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+        verify(treasureInteractionService).createTreasureInteraction(any(Treasure.class));
+    }
+}
