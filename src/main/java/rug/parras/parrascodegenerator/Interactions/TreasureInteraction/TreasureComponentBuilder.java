438bc5eb6ee7d45b426193261b3246e2aecf3f31
xoxoedg <rugyasin@gmail.com>
2021-12-04 12:00:19 +0100

    Changed Position

--- src/main/java/rug/parras/parrascodegenerator/Interactions/TreasureInteraction/TreasureComponentBuilder.java
+++ src/main/java/rug/parras/parrascodegenerator/Interactions/TreasureInteraction/TreasureComponentBuilder.java
@@ -33,10 +33,81 @@ public class TreasureComponentBuilder {
 
     public static final String CLASS_NAME_TEMPLATE = "class %sChestInteraction(StandardTreasureInteraction):\n";
 
+    public String generateTreasureInteraction(Treasure treasure) {
+        StringBuilder generatedTreasureInteraction = new StringBuilder();
+        List<String> treasureInteractionComponents = List.of(TREASURE_INTERACTION_IMPORT_TEMPLATE + "\n\n" ,generateRewardList(treasure) + "\n\n", generateClassName(treasure) + "\n\n",
+                "\t" + generateClassConstructor(treasure) + "\n\n" + "\t" +generateRetrieveChestMethod(treasure));
+        treasureInteractionComponents.forEach(generatedTreasureInteraction::append);
+        return generatedTreasureInteraction.toString();
+    }
+
     public String generateClassName(Treasure treasure) {
         return  String.format(CLASS_NAME_TEMPLATE, MapParser.convertInputToMapName(treasure.getMap()));
     }
 
+    public String generateClassConstructor(Treasure treasure) {
+        StringBuilder generatedClassConstructors = new StringBuilder();
+        List<String> classConstructorComponents = List.of(CLASS_CONSTRUCTOR_TEMPLATE, "\t\t" + generateSuperMethod(treasure),
+                INNIT_TEMPLATE, generateConstructorArguments(treasure));
+
+        classConstructorComponents.forEach(generatedClassConstructors::append);
+        return generatedClassConstructors.toString();
+    }
+
+    public String generateConstructorArguments(Treasure treasure) {
+        StringBuilder generatedConstructorArguments = new StringBuilder();
+        List<String> constructorArgumentsComponents = List.of(CONSTRUCTOR_METHOD_ARGUMENTS_TEMPLATE, generateTimeLineString(treasure), generateRewardsListName(treasure), ")");
+        constructorArgumentsComponents.stream().map(x -> !constructorArgumentsComponents.get(0).equals(x) &&
+                !constructorArgumentsComponents.get(constructorArgumentsComponents.size() -1 ).equals(x)? ", " + x: x )
+                .forEach(generatedConstructorArguments::append);
+        return generatedConstructorArguments.toString();
+    }
+
+    public String generateSuperMethod(Treasure treasure) {
+        StringBuilder generatedSuperMethod = new StringBuilder();
+        List<String> superMethodComponents = List.of(SUPER_METHOD_TEMPLATE, generateSuperMethodArguments(treasure));
+        superMethodComponents.forEach(generatedSuperMethod::append);
+        return !treasure.getMap().equals("") ? generatedSuperMethod.toString() : "";
+    }
+
+    public String generateSuperMethodArguments(Treasure treasure) {
+        StringBuilder generatedSuperMethod = new StringBuilder();
+        generatedSuperMethod.append(SUPER_METHOD_ARGUMENTS).insert(1, MapParser.convertInputToMapName(treasure.getMap()));
+        return generatedSuperMethod.toString();
+    }
+
+    public String generateTimeLineString(Treasure treasure) {
+        StringBuilder generatedTimeLineString = new StringBuilder();
+        List<String> timeLineStringComponents = List.of(TIMELINE_METHOD_STRING_TEMPLATE, treasure.getMap().toUpperCase());
+        timeLineStringComponents.forEach(generatedTimeLineString::append);
+        return "'" + generatedTimeLineString.toString() + "'";
+    }
+
+    public String generateRetrieveChestMethod(Treasure treasure) {
+        StringBuilder generatedRetrieveChestMethod = new StringBuilder();
+        List<String> retrieveChestMethodComponents = List.of(RETRIEVE_CHEST_CONTENT_TEMPLATE, "\t\t" + generateItemFinderMethod(treasure), "\n\t\t" + generateGoldEarned(treasure));
+        retrieveChestMethodComponents.forEach(generatedRetrieveChestMethod::append);
+        return generatedRetrieveChestMethod.toString();
+    }
+
+    public String generateItemFinderMethod(Treasure treasure) {
+        StringBuilder generatedItemFinderMethod = new StringBuilder();
+        List<String> itemFinderMethodComponents = List.of(ITEM_FINDER_STRING_TEMPLATE, generateItemFinderArguments(treasure));
+        itemFinderMethodComponents.forEach(generatedItemFinderMethod::append);
+        return generatedItemFinderMethod.toString();
+    }
+    public String generateItemFinderArguments(Treasure treasure) {
+        StringBuilder generatedItemFinderMethod = new StringBuilder();
+        Map<String, String> itemAmountMap = convertListToMap(filterItems(treasure), filterAmount(treasure));
+        List<String> itemFinderArgumentsComponents = new ArrayList<>(List.of("hero"));
+        itemFinderArgumentsComponents.addAll(itemAmountMap.entrySet().stream().map(x -> ", " + x.getKey() + ", " + x.getValue()).collect(Collectors.toList()));
+        itemFinderArgumentsComponents.forEach(generatedItemFinderMethod::append);
+        return "(" +generatedItemFinderMethod.toString() + ")";
+    }
+
+    public String generateGoldEarned(Treasure treasure) {
+        return  treasure.getAmountGold() > 0 ? String.format(GOLD_TEMPLATE, treasure.getAmountGold()) : "";
+    }
 
     public String generateRewardList(Treasure treasure) {
         StringBuilder generatedRewardList = new StringBuilder();
@@ -45,6 +116,24 @@ public class TreasureComponentBuilder {
         return generatedRewardList.toString();
     }
 
+    public String generateRewardsListName(Treasure treasure) {
+        StringBuilder generatedRewardsList = new StringBuilder();
+        List<String> rewardsListTemplateComponent;
+        List<String> rewardsItemComponents = filterItems(treasure);
+        if (treasure.getAmountGold() > 0) {
+            rewardsListTemplateComponent = List.of("Receive", "Gold");
+
+        } else {
+            rewardsListTemplateComponent = List.of("Receive");
+        }
+        List<String> rewardListNameComponents = Stream.concat(rewardsListTemplateComponent.stream(), rewardsItemComponents.stream())
+                .collect(Collectors.toList());
+        rewardListNameComponents.stream()
+                .map(x -> !x.equals(rewardListNameComponents
+                        .get(rewardListNameComponents.size() - 1)) ? x + "_" : x)
+                .forEach(generatedRewardsList::append);
+        return generatedRewardsList.toString().toUpperCase();
+    }
 
     public String generateListMessage(Treasure treasure) {
         StringBuilder generatedListMessage = new StringBuilder();
@@ -65,111 +154,18 @@ public class TreasureComponentBuilder {
        ? StringUtils.capitalize(x) : StringUtils.capitalize(x) + " and ").forEach(generatedListMessage::append);
         return "['" + generatedListMessage.toString() + "']";
     }
-    public String generateTimeLineString(Treasure treasure) {
-        StringBuilder generatedTimeLineString = new StringBuilder();
-        List<String> timeLineStringComponents = List.of(TIMELINE_METHOD_STRING_TEMPLATE, treasure.getMap().toUpperCase());
-        timeLineStringComponents.forEach(generatedTimeLineString::append);
-        return "'" + generatedTimeLineString.toString() + "'";
-    }
 
-    public String generateGoldEarned(Treasure treasure) {
-         return  treasure.getAmountGold() > 0 ? String.format(GOLD_TEMPLATE, treasure.getAmountGold()) : "";
-    }
-
+    //Auslagern
     public List<String> filterAmount(Treasure treasure) {
         List<Integer> amounts = List.of(treasure.getItemOneAmount(), treasure.getItemTwoAmount(), treasure.getItemThreeAmount());
         return amounts.stream().filter(x -> x != 0).map(String::valueOf).collect(Collectors.toList());
     }
-
     public List<String> filterItems(Treasure treasure) {
         List<String> items = List.of(treasure.getItemOneName(), treasure.getItemTwoName(), treasure.getItemThreeName());
         return items.stream().filter(x -> !x.equals("")).map(String::toLowerCase).collect(Collectors.toList());
     }
 
-    public String generateItemFinderArguments(Treasure treasure) {
-        StringBuilder generatedItemFinderMethod = new StringBuilder();
-        Map<String, String> itemAmountMap = convertListToMap(filterItems(treasure), filterAmount(treasure));
-        List<String> itemFinderArgumentsComponents = new ArrayList<>(List.of("hero"));
-        itemFinderArgumentsComponents.addAll(itemAmountMap.entrySet().stream().map(x -> ", " + x.getKey() + ", " + x.getValue()).collect(Collectors.toList()));
-        itemFinderArgumentsComponents.forEach(generatedItemFinderMethod::append);
-        return "(" +generatedItemFinderMethod.toString() + ")";
-    }
-
-    public String generateItemFinderMethod(Treasure treasure) {
-        StringBuilder generatedItemFinderMethod = new StringBuilder();
-        List<String> itemFinderMethodComponents = List.of(ITEM_FINDER_STRING_TEMPLATE, generateItemFinderArguments(treasure));
-        itemFinderMethodComponents.forEach(generatedItemFinderMethod::append);
-        return generatedItemFinderMethod.toString();
-    }
-
-    public String generateRetrieveChestMethod(Treasure treasure) {
-        StringBuilder generatedRetrieveChestMethod = new StringBuilder();
-        List<String> retrieveChestMethodComponents = List.of(RETRIEVE_CHEST_CONTENT_TEMPLATE, "\t\t" + generateItemFinderMethod(treasure), "\n\t\t" + generateGoldEarned(treasure));
-        retrieveChestMethodComponents.forEach(generatedRetrieveChestMethod::append);
-        return generatedRetrieveChestMethod.toString();
-    }
-    //In Common auslagern oder Utils
-
     public Map<String, String> convertListToMap(List<String> filteredItems, List<String> filteredAmounts) {
         return IntStream.range(0, filteredItems.size()).boxed().collect(Collectors.toMap(filteredItems::get, filteredAmounts::get));
     }
-
-    public String generateSuperMethodArguments(Treasure treasure) {
-        StringBuilder generatedSuperMethod = new StringBuilder();
-        generatedSuperMethod.append(SUPER_METHOD_ARGUMENTS).insert(1, MapParser.convertInputToMapName(treasure.getMap()));
-        return generatedSuperMethod.toString();
-    }
-
-    public String generateSuperMethod(Treasure treasure) {
-        StringBuilder generatedSuperMethod = new StringBuilder();
-        List<String> superMethodComponents = List.of(SUPER_METHOD_TEMPLATE, generateSuperMethodArguments(treasure));
-        superMethodComponents.forEach(generatedSuperMethod::append);
-        return !treasure.getMap().equals("") ? generatedSuperMethod.toString() : "";
-    }
-    //Refactor
-    public String generateRewardsListName(Treasure treasure) {
-        StringBuilder generatedRewardsList = new StringBuilder();
-        List<String> rewardsListTemplateComponent;
-        List<String> rewardsItemComponents = filterItems(treasure);
-        if (treasure.getAmountGold() > 0) {
-            rewardsListTemplateComponent = List.of("Receive", "Gold");
-
-        } else {
-            rewardsListTemplateComponent = List.of("Receive");
-        }
-        List<String> rewardListNameComponents = Stream.concat(rewardsListTemplateComponent.stream(), rewardsItemComponents.stream())
-                .collect(Collectors.toList());
-        rewardListNameComponents.stream()
-                .map(x -> !x.equals(rewardListNameComponents
-                        .get(rewardListNameComponents.size() - 1)) ? x + "_" : x)
-                .forEach(generatedRewardsList::append);
-        return generatedRewardsList.toString().toUpperCase();
-    }
-
-    public String generateConstructorArguments(Treasure treasure) {
-        StringBuilder generatedConstructorArguments = new StringBuilder();
-        List<String> constructorArgumentsComponents = List.of(CONSTRUCTOR_METHOD_ARGUMENTS_TEMPLATE, generateTimeLineString(treasure), generateRewardsListName(treasure), ")");
-        constructorArgumentsComponents.stream().map(x -> !constructorArgumentsComponents.get(0).equals(x) &&
-                !constructorArgumentsComponents.get(constructorArgumentsComponents.size() -1 ).equals(x)? ", " + x: x )
-                .forEach(generatedConstructorArguments::append);
-        return generatedConstructorArguments.toString();
-    }
-
-    public String generateClassConstructor(Treasure treasure) {
-        StringBuilder generatedClassConstructors = new StringBuilder();
-        List<String> classConstructorComponents = List.of(CLASS_CONSTRUCTOR_TEMPLATE, "\t\t" + generateSuperMethod(treasure),
-                INNIT_TEMPLATE, generateConstructorArguments(treasure));
-
-        classConstructorComponents.forEach(generatedClassConstructors::append);
-        return generatedClassConstructors.toString();
-    }
-
-    public String generateTreasureInteraction(Treasure treasure) {
-        StringBuilder generatedTreasureInteraction = new StringBuilder();
-        List<String> treasureInteractionComponents = List.of(TREASURE_INTERACTION_IMPORT_TEMPLATE + "\n\n" ,generateRewardList(treasure) + "\n\n", generateClassName(treasure) + "\n\n",
-                "\t" + generateClassConstructor(treasure) + "\n\n" + "\t" +generateRetrieveChestMethod(treasure));
-        treasureInteractionComponents.forEach(generatedTreasureInteraction::append);
-        return generatedTreasureInteraction.toString();
-    }
-
 }
