0ae1dab0fc29ce8a9e65e8b9fe23e5fbf9653827
xoxoedg <rugyasin@gmail.com>
2021-12-06 22:34:48 +0100

    Added ConfigurationService

--- src/main/java/rug/parras/parrascodegenerator/directoryGenerator/ConfigurationCodeGenerator/ConfigurationFileGenerator.java
+++ src/main/java/rug/parras/parrascodegenerator/directoryGenerator/ConfigurationCodeGenerator/ConfigurationFileGenerator.java
@@ -1,4 +1,14 @@
 package rug.parras.parrascodegenerator.directoryGenerator.ConfigurationCodeGenerator;
 
+import rug.parras.parrascodegenerator.directoryGenerator.FactoryFileGenerator;
+
+import java.io.File;
+
 public class ConfigurationFileGenerator {
+
+    private final String CONFIGURATION_FILE_TEMPLATE = "configuration.py";
+
+    public File createFile() {
+        return new File(CONFIGURATION_FILE_TEMPLATE);
+    }
 }
