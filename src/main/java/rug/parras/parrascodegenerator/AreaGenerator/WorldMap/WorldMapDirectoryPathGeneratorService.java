65248ab06b6adee64bc8b4f79bf4a0aa70d74f75
xoxoedg <rugyasin@gmail.com>
2021-12-07 21:27:26 +0100

    Create GameDirectory Service

--- src/main/java/rug/parras/parrascodegenerator/AreaGenerator/WorldMap/WorldMapDirectoryPathGeneratorService.java
+++ src/main/java/rug/parras/parrascodegenerator/AreaGenerator/WorldMap/WorldMapDirectoryPathGeneratorService.java
@@ -3,6 +3,8 @@ package rug.parras.parrascodegenerator.AreaGenerator.WorldMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 @Service
 public class WorldMapDirectoryPathGeneratorService {
 
@@ -11,5 +13,10 @@ public class WorldMapDirectoryPathGeneratorService {
     @Autowired
     public WorldMapDirectoryPathGeneratorService(WorldMapDirectoryPathGenerator worldMapDirectoryPathGenerator) {
         this.worldMapDirectoryPathGenerator = worldMapDirectoryPathGenerator;
+    }
+
+    public void createDirectories(String areaName) {
+        File directory = new File(worldMapDirectoryPathGenerator.createDirectoryPath(areaName));
+        directory.mkdir();
     }
 }
