4c8d1d6d2aaf78d471269f3e9ae7845ffb56637c
xoxoedg <rugyasin@gmail.com>
2021-12-07 21:53:16 +0100

    Added AreaGeneration Controller

---
+++ src/main/java/rug/parras/parrascodegenerator/AreaGenerator/blub/AreaGenerationController.java
@@ -0,0 +1,31 @@
+package rug.parras.parrascodegenerator.AreaGenerator.blub;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import rug.parras.parrascodegenerator.AreaGenerator.AreaGenerationService;
+
+@Controller
+public class AreaGenerationController {
+
+    private final AreaGenerationService areaGenerationService;
+
+    public AreaGenerationController(AreaGenerationService areaGenerationService) {
+        this.areaGenerationService = areaGenerationService;
+    }
+
+    @GetMapping("/area")
+    public String getAreaPage(Model model) {
+        model.addAttribute("area", new Area());
+        return "area";
+    }
+
+    @PostMapping
+    public String areaSubmit(@ModelAttribute Area area, Model model) {
+        areaGenerationService.createArea(area);
+        return "index";
+    }
+
+}
