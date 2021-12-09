7e4b5f0817501d19d7daeb26f90e10f1aa6a0323
xoxoedg <rugyasin@gmail.com>
2021-12-09 18:58:09 +0100

    Made Configuration List in Python file Uppercase

--- src/main/java/rug/parras/parrascodegenerator/Area/AreaGenerator/ConfigurationCodeGenerator/ConfigurationCodeGenerator.java
+++ src/main/java/rug/parras/parrascodegenerator/Area/AreaGenerator/ConfigurationCodeGenerator/ConfigurationCodeGenerator.java
@@ -8,6 +8,6 @@ public class ConfigurationCodeGenerator  {
     private final String CONFIGURATION_CODE_TEMPLATE = "ALL_%s_MAPS=[]";
 
     public String generateConfigListCode(String areaName) {
-        return String.format(CONFIGURATION_CODE_TEMPLATE, areaName);
+        return String.format(CONFIGURATION_CODE_TEMPLATE, areaName.toUpperCase());
     }
 }
