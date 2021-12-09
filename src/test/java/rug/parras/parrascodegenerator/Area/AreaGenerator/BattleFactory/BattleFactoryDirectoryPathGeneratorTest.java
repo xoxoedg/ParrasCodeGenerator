fdc5a29a6be104fe2092fc21b2d4aaa2cbb2e44b
xoxoedg <rugyasin@gmail.com>
2021-12-10 00:18:22 +0100

    Finalized BattleFactorytests

--- src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/BattleFactory/BattleFactoryDirectoryPathGeneratorTest.java
+++ src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/BattleFactory/BattleFactoryDirectoryPathGeneratorTest.java
@@ -3,12 +3,14 @@ package rug.parras.parrascodegenerator.Area.AreaGenerator.BattleFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("it")
 class BattleFactoryDirectoryPathGeneratorTest {
 
     @Autowired
@@ -16,11 +18,8 @@ class BattleFactoryDirectoryPathGeneratorTest {
 
     @Test
     void createDirectoryPath() {
-        String expectedDirPath = "src\\test\\codeGeneratorTest\\Rionnagen";
+        String expectedDirPath = "src\\test\\codeGeneratorTest\\src\\characters\\battles\\rionnagen";
         String actualDirPath = directoryPathGenerator.createDirectoryPath("Rionnagen");
-        System.out.println(actualDirPath);
         assertEquals(expectedDirPath,actualDirPath);
-        File directoryToDelete = new File(expectedDirPath);
-        directoryToDelete.delete();
     }
 }
--- src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/BattleFactory/BattleFactoryFileGeneratorTest.java
+++ src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/BattleFactory/BattleFactoryFileGeneratorTest.java
@@ -1,10 +1,36 @@
 package rug.parras.parrascodegenerator.Area.AreaGenerator.BattleFactory;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class BattleFactoryFileGeneratorTest {
 
+    @Mock
+    BattleFactoryFilepathGenerator battleFactoryFilepathGenerator;
 
+    @InjectMocks
+    BattleFactoryFileGenerator battleFactoryFileGenerator;
+
+
+    @Test
+    void createFile() throws IOException {
+        when(battleFactoryFilepathGenerator.generateFilename(anyString())).thenReturn("hello.py");
+        battleFactoryFileGenerator.createFile("hello.py");
+        verify(battleFactoryFilepathGenerator).generateFilename("hello.py");
+        File fileToDelete = new File("hello.py");
+        fileToDelete.delete();
+
+    }
 }
