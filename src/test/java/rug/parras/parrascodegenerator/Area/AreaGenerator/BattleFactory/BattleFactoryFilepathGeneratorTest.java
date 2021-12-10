78ed4f3a39e150e5cdec20454a543b26733d7eee
xoxoedg <rugyasin@gmail.com>
2021-12-10 22:16:00 +0100

    Finished tests for ConfigurationCodeGenerator

---
+++ src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/BattleFactory/BattleFactoryFilepathGeneratorTest.java
@@ -0,0 +1,24 @@
+package rug.parras.parrascodegenerator.Area.AreaGenerator.BattleFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("it")
+class BattleFactoryFilepathGeneratorTest {
+
+    @Autowired
+    BattleFactoryFilepathGenerator battleFactoryFilepathGenerator;
+
+    @Test
+    void generateFilename() {
+        String expectedFilename = "src\\test\\codeGeneratorTest\\src\\characters\\battles\\rionnagen\\rionnagen_battle_factory.py";
+        String actualFilename = battleFactoryFilepathGenerator.generateFilename("Rionnagen");
+        assertEquals(expectedFilename, actualFilename);
+
+    }
+}
--- src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/ConfigurationCodeGenerator/ConfigurationCodeGeneratorWriterTest.java
+++ src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/ConfigurationCodeGenerator/ConfigurationCodeGeneratorWriterTest.java
@@ -1,12 +1,37 @@
 package rug.parras.parrascodegenerator.Area.AreaGenerator.ConfigurationCodeGenerator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class ConfigurationCodeGeneratorWriterTest {
 
+    @Mock
+    private ConfigurationFilepathGenerator configurationFilepathGenerator;
+
+    @Mock
+    private ConfigurationCodeGenerator configurationCodeGenerator;
+
+    @InjectMocks
+    private ConfigurationCodeGeneratorWriter configurationCodeGeneratorWriter;
+
     @Test
-    void writeToFile() {
+    void writeToFile() throws IOException {
+        File fileToDelete = new File("rionnagen.py");
+        when(configurationFilepathGenerator.generateFilename("rionnagen")).thenReturn("rionnagen.py");
+        when(configurationCodeGenerator.generateConfigListCode("rionnagen")).thenReturn("test");
+        configurationCodeGeneratorWriter.writeToFile("rionnagen");
+        verify(configurationCodeGenerator).generateConfigListCode("rionnagen");
+        verify(configurationFilepathGenerator).generateFilename("rionnagen");
+        fileToDelete.delete();
     }
 }
