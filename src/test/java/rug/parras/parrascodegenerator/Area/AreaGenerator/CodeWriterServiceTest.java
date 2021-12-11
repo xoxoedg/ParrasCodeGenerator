55e9ba09acdf21d4e9b92f935845aef9f5e8d1e3
xoxoedg <rugyasin@gmail.com>
2021-12-11 01:37:42 +0100

    Added test for CodeWriterService

--- src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/CodeWriterServiceTest.java
+++ src/test/java/rug/parras/parrascodegenerator/Area/AreaGenerator/CodeWriterServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.internal.stubbing.answers.DoesNothing;
 import org.mockito.junit.jupiter.MockitoExtension;
 import rug.parras.parrascodegenerator.Area.AreaGenerator.BattleFactory.BattleFactoryCodeGeneratorWriter;
 import rug.parras.parrascodegenerator.Area.AreaGenerator.ConfigurationCodeGenerator.ConfigurationCodeGeneratorWriter;
@@ -14,10 +13,10 @@ import rug.parras.parrascodegenerator.Area.AreaGenerator.MapInteractionFactoryCo
 import rug.parras.parrascodegenerator.Area.AreaGenerator.NextMapFactory.NextMapFactoryCodeWriter;
 import rug.parras.parrascodegenerator.Area.AreaGenerator.ScenceFactory.SceneFactoryCodeWriter;
 
-import java.io.File;
 import java.io.IOException;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class CodeWriterServiceTest {
@@ -48,6 +47,21 @@ class CodeWriterServiceTest {
 
     @Test
     void writeCodeToFile() throws IOException {
+        doNothing().when(battleFactoryCodeGeneratorWriter).writeToFile("rionnagen");
+        doNothing().when(configurationCodeGeneratorWriter).writeToFile("rionnagen");
+        doNothing().when(initialMapFactoryCodeWriter).writeToFile("rionnagen");
+        doNothing().when(itemInteractionFactoryCodeWriter).writeToFile("rionnagen");
+        doNothing().when(mapAfterInteractionFactoryCodeWriter).writeToFile("rionnagen");
+        doNothing().when(nextMapFactoryCodeWriter).writeToFile("rionnagen");
+        doNothing().when(sceneFactoryCodeWriter).writeToFile("rionnagen");
+        codeWriterService.writeCodeToFile("rionnagen");
+        verify(battleFactoryCodeGeneratorWriter).writeToFile("rionnagen");
+        verify(configurationCodeGeneratorWriter).writeToFile("rionnagen");
+        verify(initialMapFactoryCodeWriter).writeToFile("rionnagen");
+        verify(itemInteractionFactoryCodeWriter).writeToFile("rionnagen");
+        verify(mapAfterInteractionFactoryCodeWriter).writeToFile("rionnagen");
+        verify(nextMapFactoryCodeWriter).writeToFile("rionnagen");
+        verify(sceneFactoryCodeWriter).writeToFile("rionnagen");
 
     }
 }
